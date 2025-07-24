1b2ee3e21e9a51c18cf2e842963acb99f8ddd4a2
VrugteVlieg <32683883+VrugteVlieg@users.noreply.github.com>
2025-07-24 19:56:47 +0200

    set runtime at startup

--- bin/dlox.dart
+++ bin/dlox.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
 import 'package:dlox/dlox.dart' as dlox;
+import 'package:dlox/runner.dart';
 import 'package:logging/logging.dart';
 
 void main(List<String> args) {
   initLoggers();
+  dlox.setRuntime(DefaultRunner());
   // log("Test", level: Level.INFO.value);
   if (args.length > 1) {
     print("Usage dlox [script]");
@@ -18,7 +20,7 @@ void main(List<String> args) {
 
 void initLoggers() {
   hierarchicalLoggingEnabled = true;
-  Logger.root.level = Level.FINEST;
+  Logger.root.level = Level.FINE;
   Logger.root.onRecord.listen(
       (record) => print("[${record.level}]@${record.time}: ${record.message}"));
 }
